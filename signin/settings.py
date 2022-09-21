dac6ef3d40cc9cd42df03b7ef4b4bd581fea46e4
madhu0200 <mmadhusudhanr4@gmail.com>
2022-09-21 23:15:29 +0530

    Update settings.py

--- signin/settings.py
+++ signin/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'signin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DBNAME',
-        'USER':'root',
-        'PASSWORD':'PASSWORD',
-        'HOST':'MYSQLDATABASE',
-        'PORT':'3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': 'sqlite3',
     }
@@ -130,5 +126,5 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
